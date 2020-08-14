3365386ed992b9f994750ce7610b45562ff1bee3
abirhalwa <abir.halwa@gmail.com>
2020-08-13 21:58:32 -0400

    Updates

---
+++ precache-manifest.253281629c50a76440d280882c327404.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17b55751421cb4c222328dd49a6372a",
+    "url": "/project5/index.html"
+  },
+  {
+    "revision": "2a032d274a34262fa517",
+    "url": "/project5/static/css/main.07ba1411.chunk.css"
+  },
+  {
+    "revision": "18380df6cbbe7cd7c141",
+    "url": "/project5/static/js/2.a7e681a8.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/project5/static/js/2.a7e681a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a032d274a34262fa517",
+    "url": "/project5/static/js/main.bce5b35b.chunk.js"
+  },
+  {
+    "revision": "392a38de9e3f2105cc72",
+    "url": "/project5/static/js/runtime-main.99ddbd30.js"
+  },
+  {
+    "revision": "a485c0ea5cfb26eb9f813f7cf84d1a0f",
+    "url": "/project5/static/media/0d3e4a84ba9f77eef545736731c9e2a8.a485c0ea.jpg"
+  }
+]);
